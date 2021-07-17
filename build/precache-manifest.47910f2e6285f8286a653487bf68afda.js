bd017f985c941a36fa188a33fba75283582f41c7
Msony55 <52307314+Msony55@users.noreply.github.com>
2021-07-17 10:33:17 +0530

    Add files via upload

---
+++ build/precache-manifest.47910f2e6285f8286a653487bf68afda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c117321c1940a1111f807a2cb9d57e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30f126406a52ef872a8d",
+    "url": "/static/css/2.ffe872a2.chunk.css"
+  },
+  {
+    "revision": "ddc1d5fd720125177ffb",
+    "url": "/static/css/main.686842f3.chunk.css"
+  },
+  {
+    "revision": "30f126406a52ef872a8d",
+    "url": "/static/js/2.63bbf128.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.63bbf128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc1d5fd720125177ffb",
+    "url": "/static/js/main.bc3b19d8.chunk.js"
+  },
+  {
+    "revision": "b7f25be1b0d205ef19b7",
+    "url": "/static/js/runtime-main.2b2479c5.js"
+  }
+]);
